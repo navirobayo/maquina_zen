cf43388e8f92f6377b3c28ab52cc75b75c679ee0
Ivan <101908819+host-2077@users.noreply.github.com>
2023-05-11 14:05:28 -0500

    Add files via upload

---
+++ More/first_idea.py
@@ -0,0 +1,20 @@
+print("Bienvenido a 81mAL5.")
+userName = input("Para iniciar ingresa tu nombre: ")
+print("Bienvenido", userName)
+print("Si estás acá es porque estás buscando respuestas que solo tú podrás responder. De todas maneras, \nTODO es solo un truco y este simple programa de computadora te permitirá entender aquello.")
+print("---------------------------------------------------------------------------\n---------------------------------------------------------------------------")
+print("Nuestros servidores disponen han logrado encontrar 3 registros de Maestros 'Zen' \n¿Con cual de ellos quisieras hablar?")
+print("Dogen > > > ")
+print("Bodhidharma > > >")
+print("Milarepa > > >")
+dogenSalute = "saludos"
+userPath = input("Quiero hablar con:")
+if userPath == "Dogen":
+    print("El maestro Dogen ha ingresado a la sala de chat.")
+    for i in dogenSalute:
+        print(i)
+    for i in userName:
+        print(i)
+    print("Dogen dice:", userName + ",", "si eres incapaz de encontrar la verdad justo donde estás, ¿donde más esperas encontrarla?")
+    responseDogen = input("¿Que respondes?")
+    print("Dogen dice: ")
